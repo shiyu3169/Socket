7cf5eae9115df53631792882e829b913ff66dbac
Shiyu Wang <cliff316912991@gmail.com>
2017-02-24 01:50:17 -0500

    updated tcl files

--- Project_3/exp.tcl
+++ Project_3/exp.tcl
@@ -1,25 +1,9 @@
 #Create a simulator object
 set ns [new Simulator]
 
-# #Define different colors for data flows (for NAM)
-# $ns color 1 Blue
-# $ns color 2 Red
-
-#Open the NAM trace file
-# set nf [open out.nam w]
-# $ns namtrace-all $nf
-
-set traceFile [open test-trace.tr w]
-$ns trace-all $traceFile
-$ns trace-all stdout
-
-#Define a 'finish' procedure
 proc finish {} {
-        global ns nf traceFile
+        global ns 
         $ns flush-trace
-        #Close the NAM trace file
-        # close $nf
-        close $traceFile
         exit 0
 }
 
@@ -28,7 +12,6 @@ set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
-
 
 #Retrieve the command line arguments
 set arg1 [lindex $argv 0] 
@@ -39,17 +22,11 @@ $ns duplex-link $n1 $n2 10Mb 10ms DropTail
 $ns duplex-link $n2 $n3 10Mb 10ms DropTail
 $ns duplex-link $n3 $n4 10Mb 10ms DropTail
 
-#Set Queue Size of link (n2-n3) to 10
+#trace the traffic between n2 and n3
+$ns trace-queue $n2 $n3 stdout
+
+#Set Queue Size of link (n2-n3) to 5
 $ns queue-limit $n2 $n3 5
-
-#Give node position (for NAM)
-# $ns duplex-link-op $n0 $n2 orient right-down
-# $ns duplex-link-op $n1 $n2 orient right-up
-# $ns duplex-link-op $n2 $n3 orient right
-
-#Monitor the queue for link (n2-n3). (for NAM)
-# $ns duplex-link-op $n2 $n3 queuePos 0.5
-
 
 #Setup a TCP connection
 set tcp [new Agent/$arg1]
@@ -82,17 +59,13 @@ $cbr set packet_size_ 1000
 $cbr set rate_ $arg2
 $cbr set random_ false
 
-
 #Schedule events for the CBR and FTP agents
 $ns at 0.1 "$cbr start"
 $ns at 1.0 "$ftp start"
 $ns at 8.0 "$ftp stop"
 $ns at 9.0 "$cbr stop"
 
-#Detach tcp and sink agents (not really necessary)
-# $ns at 4.5 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n3 $sink"
-
-#Call the finish procedure after 5 seconds of simulation time
+#Call the finish procedure after 10 seconds of simulation time
 $ns at 10 "finish"
 
 #Print CBR packet size and interval
