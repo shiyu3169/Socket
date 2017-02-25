c7064a19b685803403529aec7a93c9180ec85f34
BenWng <wnghan@gmail.com>
2017-02-24 19:25:01 -0500

    Exp1 is kind of finished

--- Project_3/src/exp1.java
+++ Project_3/src/exp1.java
@@ -9,33 +9,6 @@ import java.util.regex.Pattern;
  */
 
 
-/*
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-public class RunShellCommandFromJava {
-
-    public static void main(String[] args) {
-
-        String command = "ping -c 3 www.google.com";
-
-        Process proc = Runtime.getRuntime().exec(command);
-
-        // Read the output
-
-        BufferedReader reader =
-              new BufferedReader(new InputStreamReader(proc.getInputStream()));
-
-        String line = "";
-        while((line = reader.readLine()) != null) {
-            System.out.print(line + "\n");
-        }
-
-        proc.waitFor();
-
-    }
-}
- */
 
 public class exp1 {
 
@@ -48,7 +21,7 @@ public class exp1 {
     }
 
 
-    public static void runTCLFile(String command){
+    public static void runTCLFile(String command, int CBRRate, expOneDataPointsCollector collector){
         String line="";
         try{
             Process proc=Runtime.getRuntime().exec(command);
@@ -61,12 +34,14 @@ public class exp1 {
                     lineObject newLine=new lineObject(line);
                     analyzer.processNewLine(newLine);
                 }
-
             }
-
+            collector.addDropRate(CBRRate,analyzer.getDropRate());
+            collector.addThroughput(CBRRate,analyzer.getThroughput());
+            collector.addRTT(CBRRate,analyzer.getAverageRTT());
+            /*
             System.out.println(analyzer.getAverageRTT());
             System.out.println(analyzer.getDropRate());
-            System.out.println(analyzer.getThroughput());
+            System.out.println(analyzer.getThroughput());*/
 
         }catch (IOException e){
             System.out.println(e);
@@ -75,27 +50,17 @@ public class exp1 {
 
     public static void main(String[] args){
 
-        runTCLFile("ns exp.tcl TCP 10mb");
+        expOneDataPointsCollector collector=new expOneDataPointsCollector();
 
-        /*
-        String test1="r 5.5556 1 2 tcp 1040 ------- 1 0.0 2.0 1100 5601";
-        String test2="- 5.558032 2 3 tcp 1040 ------- 1 0.0 2.0 1101 5603";
-        String test3="- 5.558032 2 3 tcp 1040 ------- 1 0.0 250 1101 5603";
-        System.out.println(findLinesOfInterest(test1));
-        System.out.println(findLinesOfInterest(test2));
-        System.out.println(findLinesOfInterest(test3));
-        */
+        int initialCBRRate=0;
+        int interval=1;
 
 
-
-
-
-
-
+        for (int rate=initialCBRRate;rate<=10;rate+=interval) {
+            String command="ns exp0.tcl TCP "+rate+"mb";
+            runTCLFile(command, rate, collector);
+        }
+        System.out.println(collector.toString());
 
     }
-
-
-
-
 }
