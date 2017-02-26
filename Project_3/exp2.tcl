5ef3ffa2d4843510661d087e02c31544879b0cd7
Shiyu Wang <cliff316912991@gmail.com>
2017-02-26 16:17:45 -0500

    updated data and tcl2

--- Project_3/exp2.tcl
+++ Project_3/exp2.tcl
@@ -31,9 +31,6 @@ $ns duplex-link $n5 $n2 10Mb 10ms DropTail
 $ns duplex-link $n3 $n6 10Mb 10ms DropTail
 $ns duplex-link $n4 $n3 10Mb 10ms DropTail
 
-#Set Queue Size of link (n2-n3) to 10
-$ns queue-limit $n2 $n3 10
-
 #Setup a TCP connection from N1 to N4
 set tcp [new Agent/$arg1]
 $tcp set class_ 2
