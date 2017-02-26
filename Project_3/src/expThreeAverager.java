ed7911e27fad3bc031d7b7dd07338c120966f6f4
BenWng <wnghan@gmail.com>
2017-02-25 19:01:33 -0500

    Add averager

---
+++ Project_3/src/expThreeAverager.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ben_Big on 2/25/17.
+ */
+public class expThreeAverager {
+    /*
+    public void addResult(double time, List<Float> result){
+        if (time<=TCPStartTime) return;
+        if (!timeToResult.containsKey(time)) {
+            List<Float> newResult=new ArrayList<>();
+            for (int i=0;i<result.size();i++){
+                newResult.add(i,result.get(i)/numTrials);
+            }
+            timeToResult.put(time, newResult);
+        }
+        else{
+            List<Float> newResult=new ArrayList<>();
+            for (int i=0;i<result.size();i++){
+                newResult.add(i,result.get(i)/numTrials+ timeToResult.get(time).get(i));
+            }
+            timeToResult.replace(time,newResult);
+        }
+    }*/
+}
