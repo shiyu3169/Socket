100a554077afc4877c6a7b48c479835234ee7c49
BenWng <wnghan@gmail.com>
2017-03-19 18:17:49 -0400

    Halfway in IPv4Packet.py

--- Project_4/Zhongxi_ip/IPv4Packet.py
+++ Project_4/Zhongxi_ip/IPv4Packet.py
@@ -1,4 +1,4 @@
-import socket
+import struct
 import socket
 from random import randint
 
@@ -12,6 +12,10 @@ ID_GENERATOR=cyclic_generator()
 
 
 class IPv4Packet:
+
+    PACKET_MIN_SIZE=20
+    PACKET_MAX_SIZE=65535
+
     def __init__(self,source,destination,data):
 
 
@@ -57,6 +61,42 @@ class IPv4Packet:
         try:
             self.total_length+=len(self.data)
         except:
-            raise Exception("Data of current packet is empty or not in the correct form (byte literal)")
+            print("Data of current packet is empty or not in the correct form (byte literal)")
 
+
+    @classmethod
+    def generate_packet_from_received_bytes(cls,bytes):
+        if len(bytes)<IPv4Packet.PACKET_MIN_SIZE:
+            print("Not enough bytes to build a packet") #ToDo: should the message just be printed or raised as an Exception
+            return None
+        elif len(bytes)>IPv4Packet.PACKET_MAX_SIZE:
+            print("Too many bytes for one packet") #ToDo: should the message just be printed or raised as an Exception
+            return None
+
+        first_five_words_of_header=struct.unpack("!BBHHHBBH4s4s",bytes)
+        source_ip_of_bytes=first_five_words_of_header[8]
+        destination_ip_of_bytes=first_five_words_of_header[9]
+        temporary_data=b""
+        packet_generated_from_bytes=cls(socket.inet_ntoa(source_ip_of_bytes),
+                                        socket.inet_ntoa(destination_ip_of_bytes),
+                                        temporary_data)
+        packet_generated_from_bytes.version=first_five_words_of_header[0]>>4
+        packet_generated_from_bytes.ihl=first_five_words_of_header[0]& 0x0f
+        packet_generated_from_bytes.dscp=first_five_words_of_header[1]>>2
+        packet_generated_from_bytes.ecn=first_five_words_of_header[1]& 0x03
+        packet_generated_from_bytes.total_length=first_five_words_of_header[2] & 0xffff
+
+
+
+
+
+
+    def convert_packet_to_bytes(self):
+
+
+
+
+
+
+    def checksum(self):
 
