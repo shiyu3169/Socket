e7072cb2abc3ac0678d7904bc4815ef43b309ea0
Shiyu Wang <cliff316912991@gmail.com>
2017-03-27 02:55:32 -0400

    update more on TCP Packet

--- Project_4/Shiyu_TCP/TCPPacket.py
+++ Project_4/Shiyu_TCP/TCPPacket.py
@@ -10,42 +10,37 @@ class TCPPacket:
         self.ack_num = ack_num
         self.data_offset = 5
         self.reserved = 0
-
-        # Control Bits
         self.urg = 0
         self.ack = 0
         self.psh = 0
         self.rst = 0
         self.syn = 0
         self.fin = 0
-        self.window = 4096
-
+        self.window = 0xFFFF
         self.check = 0
-        self.urgptr = 0
+        self.urgent = 0
         self.options = []
-
-        self.bytes = None
         self.data = data
 
     @classmethod
-    def unpack(cls, data, src_addr, dest_addr):
+    def unpack(cls, data, s_addr, d_addr):
         """
-        Create a TCP packet from bytes aquired via the network (or any other source).
+        Unpack received data in bytes and create a TCP packet
         """
         header = struct.unpack("!HHLL2sHHH", data[0:20])
-        src_port = header[0]
-        dest_port = header[1]
+        s_port = header[0]
+        d_port = header[1]
         seq = header[2]
         ack_num = header[3]
-        dataoffset_rsvd_flags = int.from_bytes(header[4], 'big')
+        data_offset_reserved_flags = int.from_bytes(header[4], 'big')
         window = header[5]
         check = header[6]
-        urgent_ptr = header[7]
+        urgent = header[7]
 
-        data_offset = dataoffset_rsvd_flags >> 12
-        reserved = (dataoffset_rsvd_flags >> 6) & 0b111111
+        data_offset = data_offset_reserved_flags >> 12
+        reserved = (data_offset_reserved_flags >> 6) & 0b111111
 
-        flags = dataoffset_rsvd_flags & 0b111111
+        flags = data_offset_reserved_flags & 0b111111
         urg = (flags & 0x20) >> 5
         ack = (flags & 0x10) >> 4
         psh = (flags & 0x08) >> 3
@@ -53,7 +48,7 @@ class TCPPacket:
         syn = (flags & 0x02) >> 1
         fin = flags & 0x01
 
-        packet = cls((src_addr, src_port), (dest_addr, dest_port), seq, ack_num, b'')
+        packet = cls((s_addr, s_port), (d_addr, d_port), seq, ack_num, b'')
         packet.data_offset = data_offset
         packet.reserved = reserved
         packet.urg = urg
@@ -64,99 +59,74 @@ class TCPPacket:
         packet.fin = fin
         packet.window = window
         packet.check = check
-        packet.urgptr = urgent_ptr
+        packet.urgent = urgent
 
-        packet.set_options(data[20: data_offset * 4])
-        packet.data = data[data_offset * 4:]
-
-        packet.bytes = data
-
-        return packet
-
-    @property
-    def flags(self):
-        """
-        Convert the flags to a single int.
-        """
-        flags = self.fin + \
-                (self.syn << 1) + \
-                (self.rst << 2) + \
-                (self.psh << 3) + \
-                (self.ack << 4) + \
-                (self.urg << 5)
-        return flags
-
-    def set_options(self, options_bytes):
-        """
-        Set the options from bytes
-        """
+        #set options
         options = []
         index = 0
-        offset_bytes = 4 * (self.data_offset - 5)
+        option_data = data[20: data_offset * 4]
+        offset_bytes = 4 * (packet.data_offset - 5)
         while index < offset_bytes:
-            byte = options_bytes[index]
-            if byte == 0 or byte == 1:
-                options.append({'kind': byte})
+            option = option_data[index]
+            if option == 0 or option == 1:
+                options.append({'kind': option})
                 index += 1
                 break
-            elif byte == 2:
-                length = options_bytes[index + 1]
-                value = int.from_bytes(options_bytes[index + 2: index + length], 'big')
+            elif option == 2:
+                length = option_data[index + 1]
+                value = int.from_bytes(option_data[index + 2: index + length], 'big')
                 option = {'kind': 2, 'length': length, 'value': value}
                 options.append(option)
                 index += 2 + length
             else:
                 pass
-        self.options = options
+        packet.options = options
 
-    def generate_options(self):
-        """
-        Convert the options to bytes
-        """
-        if self.data_offset == 5:
-            return b''
+        packet.data = data[data_offset * 4:]
 
-        options = b''
-        for option in self.options:
-            if option['kind'] == 0:
-                options += b'\x00'
-            elif option['kind'] == 1:
-                options += b'\x01'
-            elif option['kind'] == 2:
-                l = option['length']
-                options += b'\x02' + l.to_bytes(1, 'big') + option['value'].to_bytes(l - 2, 'big')
-
-        # Add padding to reach a multiple of 4 bytes
-        pad_size = 4 - len(options) % 4
-        options += (0).to_bytes(pad_size, 'big')
-
-        return options
+        return packet
 
     def build(self):
         """
-        build the data in byte
+        The main purpose of this class.
+        :return: Bytes to send over the network.
         """
-        dataoffset_resvd_flags = 0
-        dataoffset_resvd_flags = dataoffset_resvd_flags | ((self.data_offset & 0b1111) << 12)
-        dataoffset_resvd_flags = dataoffset_resvd_flags | (self.flags & 0b111111)
-        dataoffset_resvd_flags = dataoffset_resvd_flags.to_bytes(2, 'big')
+        flags = self.fin + (self.syn << 1) + (self.rst << 2) + (self.psh << 3) + (self.ack << 4) + (self.urg << 5)
+        data_offset_reserved_flags = 0
+        data_offset_reserved_flags = data_offset_reserved_flags | ((self.data_offset & 0b1111) << 12)
+        data_offset_reserved_flags = data_offset_reserved_flags | (flags & 0b111111)
+        data_offset_reserved_flags = data_offset_reserved_flags.to_bytes(2, 'big')
 
-        header = struct.pack('!HHLL2sHHH', self.src[1], self.dest[1], self.seq, self.ack_num, dataoffset_resvd_flags,
-                             self.window, self.check, self.urgptr)
+        header = struct.pack('!HHLL2sHHH', self.src[1], self.dest[1], self.seq, self.ack_num, data_offset_reserved_flags,
+                             self.window, self.check, self.urgent)
 
-        return header + self.generate_options() + self.data
+        options = b''
+        if self.data_offset != 5:
+            for option in self.options:
+                if option['kind'] == 0:
+                    options += b'\x00'
+                elif option['kind'] == 1:
+                    options += b'\x01'
+                elif option['kind'] == 2:
+                    l = option['length']
+                    options += b'\x02' + l.build(1, 'big') + option['value'].build(l - 2, 'big')
+            # Add padding to reach a multiple of 4 bytes
+            pad_size = 4 - len(options) % 4
+            options += (0).to_bytes(pad_size, 'big')
+
+        return header + options + self.data
 
     def checksum(self):
         """
-        Set the checksum to the value returned by checksum().
+        calculate and set checksum of the packet
         """
         pseudo_header = struct.pack("!4s4sBBH", socket.inet_aton(self.src[0]), socket.inet_aton(self.dest[0]), 0, 6,
-                                    len(self))
+                                    self.data_offset * 4 + len(self.data))
         check = 0
         bytes = pseudo_header + self.build()
         bytes_length = len(bytes)
         i = 0
-        
+
         while bytes_length > 1:
             # Add all the shorts together
             b = bytes[i:i + 2]
@@ -175,10 +145,3 @@ class TCPPacket:
         # Flip the sum
         check = ~check & 0xffff
         self.check = check
-
-    def __len__(self):
-        """
-        return The number of bytes in this packet
-        """
-        data_len = len(self.data)
-        return self.data_offset * 4 + data_len
