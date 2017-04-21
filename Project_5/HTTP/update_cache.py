dac37942be48b236b1c8ed1c74ddb767fc752d1a
Student <student@CS5600-f15-Ubuntu32.defaultdomain>
2017-04-20 23:05:42 -0400

    multi thread add

--- Project_5/HTTP/update_cache.py
+++ Project_5/HTTP/update_cache.py
@@ -1,10 +1,21 @@
 import httplib
 import zlib
+import Queue
+from threading import Thread
 
-# Add disk files' names into set
 
 origin = "ec2-54-166-234-74.compute-1.amazonaws.com"
 port = 8080
+web_names = Queue.Queue()
+
+def build_queue():
+    # Download and compress based on pathname
+    f = open('pathname', 'rb')
+    line = f.readline()
+    while line != "":
+        name = line.strip()
+        web_names.put(name)
+        line = f.readline()
 
 
 def download(name):
@@ -17,21 +28,31 @@ def download(name):
     return content
 
 
-def compress(file, name):
-    str_object2 = zlib.compress(file, 9)
+def compress(name):
+    str_object2 = zlib.compress(download(name), 9)
     f = open('./zip/' + name + '.Z', 'wb')
     f.write(str_object2)
     f.close()
 
 
+def compress_forever():
+    while not web_names.empty():
+        compress(web_names.get())
+
+
+def threaded_function(num_threads):
+    for i in range(num_threads):
+        worker = Thread(target=compress_forever)
+        worker.setDaemon(True)
+        worker.start()
+    worker.join()
+
+
 def main():
-    f = open('pathname', 'rb')
-    line = f.readline()
-    while line != "":
-        name = line.strip()
-        compress(download(name), name)
-        line = f.readline()
-    return
+    # build a queue of names
+    build_queue()
+    threaded_function(10)
+
 
 if __name__=="__main__":
     main()
